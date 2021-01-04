717f72e959cafb86239b079c1ba0fa59fed347b6
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-03 21:49:42 -0800

    Add current date with moment.js

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+//Create Rows for each time slot 9-5
+
+//eventListener for onclick to edit each row
+
+
+
+//save button to save with localstorage
+
+
+
+//update row color based on current time
+
+
+
+
+
+});
