3e1ef95e6dfc166d2bad6c11782d4c9991e7333f
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-04 23:25:48 -0800

    Add event listener to Save button for local storage

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ let hourDiv = $('#hour-container');
 
 hourDiv.empty();
 
-//Create Rows for each time slot 9-5
+//Create Rows for each time slot 9-5 with for loop
 for (let hour = 9; hour <= 17; hour++){
     let rowDiv = $('<div>');
     rowDiv.addClass('row time-block');
@@ -16,7 +16,8 @@ for (let hour = 9; hour <= 17; hour++){
     //Create Row elements and add classes
     let timeDiv = $('<div>').addClass('col-md-1');
     let textAreaDiv = $('<textarea>').addClass('description col-md-10');
-    let saveButton = $('<button>').addClass('save-btn col-md-1');
+    // set attribute to be called on for localStorage
+    let saveButton = $('<button>').addClass('save-btn col-md-1').attr('id', hour);
    
     let displayHour = 0;
     let ampm = "";
@@ -41,18 +42,25 @@ for (let hour = 9; hour <= 17; hour++){
     
 }
 
+//Save button eventlisten to store with local storage
+// $(this) is a reference to '.save-btn', which calls the function
+// Grabs id associated with specific time slot
+// .val used to return the value of an input field/text area.
+$('.save-btn').on('click', function(){
+    let time = $(this).attr('id')
+    let value = $(this).siblings('textarea').val()
+    console.log(time, value);
+    localStorage.setItem(time, value);
+
+});
+
+// for loop to get stored local storage for each row
+// Template literals to consolidate multiple strings
+for (let i = 9; i <= 17; i++){
+    $(`#${i}`).siblings('textarea').val(localStorage.getItem(i));
+}
 
 
-//eventListener for saveButton onclick to edit input each row
-
-
-
-
-//save button to save with localstorage
-
-
-
-//save button to save with localstorage
 
 
 
